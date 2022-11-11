2e895cf22f4d192939d4d960d897d0478022b153
caden-deutscher <caden.deutscher@outlook.com>
2022-11-10 18:42:36 -0800

    Removed login added routes for location based queries

--- flaskapi/apive/app.py
+++ flaskapi/apive/app.py
@@ -1,6 +1,17 @@
 from flask import Flask, abort, request, redirect, render_template, request, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from models import db
 
 app = Flask(__name__)
+ 
+db = SQLAlchemy()
+
+login = LoginManager()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<db_name>.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 
 #Allowed IP address for input
 trusted_proxies = ('42.42.42.42', '82.42.82.42', '127.0.0.1')
@@ -23,22 +34,33 @@ def hello_world():
     return render_template("main_page.html")
 
 #Sign-in for admin    
-@app.route("/signin")
-def hello_world():
-    return "this is the sign in page"
+@app.route("/signin", methods = ['POST', 'GET'])
+def sign_in():
+    return render_template("signin.html")
 
 #Query Sensor Data Return JSON for specific time line
 @app.route("/<sensor>/<dayf>/<monthf>/yearf>/<dayt>/<montht>/yeart>")
 def getSpecificData(sensor,dayf,monthf,yearf,dayt,montht,yeart):
     return "Successful retrieval"
 
+#Query Data from a specific room
+@app.route("/<building>/<room>/<datef>/<datet>")
+def room_query(building,room,datef,datet):
+    return "Successful retrival"
+
+#Query Data from a specific building
+@app.route("/<building>/<datef>/<datet>")
+def building_query(building,datef,datet):
+    return "Successful retrival"
+
 #Query Sensor Data for the current day
 @app.route("/<sensor>/")
 def getDayData(sensor):
     return "Successful retrieval"
 
+
 #Add Data to DB
-@app.route("/<sensor>/<dtype>/<value>/<day>/<month>/<year>")
-def addData(sensor,dtype,value,day,month,year):
+@app.route("/<sensor>/<dtype>/<building>/<room>/<value>/<day>/<month>/<year>")
+def addData(sensor,dtype,building, room, value,day,month,year):
     return "Successful Addition"
 
