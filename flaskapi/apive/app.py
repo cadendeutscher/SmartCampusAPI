6264818bcc95cfcb23ba4460bb065695dff105b4
caden-deutscher <caden.deutscher@outlook.com>
2022-11-16 18:17:46 -0800

    Creates Table and Allows additions

--- flaskapi/apive/app.py
+++ flaskapi/apive/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, abort, request, redirect, render_template, request, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import json
 
 app = Flask(__name__)
  
@@ -24,8 +26,8 @@ class SensorData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     vtype = db.Column(db.String(80), nullable = False)
     value =db.Column(db.String(80), nullable = False)
-    date = db.Column(db.DateTime, nullable = False)
-    sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'))
+    date = db.Column(db.String(80), nullable = False)
+    sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'), nullable = False)
    
 
 #Allowed IP address for input...This are mocked IPs right now!
@@ -58,8 +60,8 @@ def sign_in():
     return render_template("signin.html")
 
 #Query Sensor Data Return JSON for specific time line
-@app.route("/<sensor>/<dayf>/<monthf>/yearf>/<dayt>/<montht>/yeart>")
-def getSpecificData(sensor,dayf,monthf,yearf,dayt,montht,yeart):
+@app.route("/<sensor>/<datef>/<datet>")
+def getSpecificData(sensor,datef,datet):
     return "Successful retrieval"
 
 #Query Data from a specific room
@@ -72,14 +74,29 @@ def room_query(building,room,datef,datet):
 def building_query(building,datef,datet):
     return "Successful retrival"
 
+
+
 #Query Sensor Data for the current day
 @app.route("/<sensor>/")
 def getDayData(sensor):
+
+    #Query Data for todays data
+    tday = datetime.now().strftime("20%y-%m-%d")
+    SensorData.query.filter((SensorData.date == tday)).all()
+
+    #Query the sensor which the data is tied to
+
     return "Successful retrieval"
 
 
 #Add Data to DB
-@app.route("/<sensor>/<dtype>/<building>/<room>/<value>/<day>/<month>/<year>")
-def addData(sensor,dtype,building, room, value,day,month,year):
+@app.route("/<sensor>/<dtype>/<building>/<room>/<value>/<date>")
+def addData(sensor,dtype,building, room, value,date):
+    #Create sensor and data objects and commit them to the DB
+    asensor = Sensor(sname=sensor,building=building,room=room)
+    sensordata = SensorData(vtype=dtype,value=value,date=date, sensor=asensor)
+    db.session.add(asensor)
+    db.session.add(sensordata)
+    db.session.commit()
     return "Successful Addition"
 
