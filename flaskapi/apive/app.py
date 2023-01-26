339492aa5d6c82b68f9a738b3c5660c143eec109
caden-deutscher <caden.deutscher@outlook.com>
2023-01-25 19:18:28 -0800

    Relationship Model Add

--- flaskapi/apive/app.py
+++ flaskapi/apive/app.py
@@ -19,13 +19,23 @@ db.init_app(app)
 #Source of help: https://flask-sqlalchemy.palletsprojects.com/en/2.x/models/
 #More Help Here: https://www.digitalocean.com/community/tutorials/how-to-use-one-to-many-database-relationships-with-flask-sqlalchemy#step-4-displaying-a-single-post-and-its-comments
 #This is a one to many relational DB Sensors are linked to their children data
+class Building(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    building = db.Column(db.String(80), nullable = False)
+    datas = db.relationship('Room', backref='building', lazy=True)
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room = db.Column(db.Integer, nullable = False)
+    datas = db.relationship('Sensor', backref='building', lazy=True)
+    building_id = db.Column(db.Integer, db.ForeignKey('building.id'), nullable = False)
+
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sname = db.Column(db.String(80), nullable=False, unique=True)
-    building = db.Column(db.String(80), nullable = False)
-    room = db.Column(db.Integer, nullable = False)
     vtype = db.Column(db.String(80), nullable = False)
     datas = db.relationship('SensorData', backref='sensor', lazy=True)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable = False)
 
 
 class SensorData(db.Model):
