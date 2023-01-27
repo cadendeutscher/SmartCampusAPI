9a0047351ed43543a64e681f765076baa69aaeab
deutsche23@up.edu <deutsche23@up.edu>
2023-01-26 18:40:40 -0800

    Test Docs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4MDU0OA/spectral.js?branch_slug=main&token=f7d92496-f4e4-4308-a4e1-97c7a01c36ce";
+export default { extends: ruleset };
